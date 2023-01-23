c01a2330423b571a8f7d1ec6026b399cbb206742
sergivonavi <sergivonavi@gmail.com>
2023-01-23 07:16:27 +0300

    Fixed dismiss animation
    Fixes #6.
    Thanks to @JonathanImperato.

--- library/src/main/java/com/sergivonavi/materialbanner/Banner.java
+++ library/src/main/java/com/sergivonavi/materialbanner/Banner.java
@@ -957,7 +957,8 @@ public class Banner extends ViewGroup implements BannerInterface {
                 MarginLayoutParams layoutParams = (MarginLayoutParams) getLayoutParams();
                 layoutParams.bottomMargin = mMarginBottom;
                 setLayoutParams(layoutParams);
-                setTranslationY(0);
+                // #7 Fix dismiss animation
+                //setTranslationY(0);
             }
 
             if (isShown()) {
